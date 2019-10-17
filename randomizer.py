a10b09747b04b8de75cfa55a82e1f054a6070432
JarheadHME <JarheadHME@Gmail.com>
2019-10-17 16:51:16 -0400

    Remove active locations from spoiler log header as it's entirely unnecessary

--- randomizer.py
+++ randomizer.py
@@ -740,7 +740,7 @@ class Randomizer:
     non_disabled_options = [
       name for name in self.options
       if self.options[name] not in [False, [], {}, OrderedDict()]
-      and name != "randomized_gear" # Just takes up space
+      and name != "randomized_gear" and name != "active_locations" # Just takes up space
     ]
     option_strings = []
     for option_name in non_disabled_options:
